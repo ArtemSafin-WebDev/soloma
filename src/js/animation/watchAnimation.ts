e4caca07cfbbcc23a1575ce51d2d1ec326995263
gilyazov <gilyazov.aydar@gmail.com>
2025-06-10 14:45:19 +0300

    анимация в блоке с видео

--- src/js/animation/watchAnimation.ts
+++ src/js/animation/watchAnimation.ts
@@ -4,14 +4,24 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger)
 
 export default function initWatchAnimation() {
-  gsap.timeline({
+  const tl = gsap.timeline({
     scrollTrigger: {
       trigger: '.watch',
       start: "top 30%",
       toggleActions: "play none none none" // play при входе, ничего при выходе
     }
-  })
-  .to('.watch__bg-image', { opacity: 1, duration: 1.2 })
-  .to('.watch__heading', { opacity: 1, y: 0, duration: 0.8 }, '-=0.5')
-  .to('.watch__text', { opacity: 1, y: 0, duration: 1 }, '-=0.3');
+  });
+  tl.to('.watch__bg-image', { opacity: 1, duration: 1.2 });
+  tl.to('.watch__text', { opacity: 1, y: 0, duration: 0.5 }, '-=0.3');
+  tl.to('.watch__link', { opacity: 1, y: 0, duration: 0.5}, '+=0.2');
+
+  tl.fromTo('.watch__decor--left',
+      { x: 0, opacity: 0 },
+      { x: -20, opacity: 1, duration: 0.7 }, '<'
+  ).fromTo('.watch__decor--right',
+      { x: 0, opacity: 0 },
+      { x: 20, opacity: 1, duration: 0.7 }, '<'
+  );
+
+  tl.to('.watch__heading', { opacity: 1, y: 0, duration: 0.8 }, '-=0.5');
 }
