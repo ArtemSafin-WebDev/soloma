493762fe05acb729ef7546a7c7598f66eb89577b
Artem Safin <artemsafin.webdev@gmail.com>
2025-06-10 15:04:32 +0300

    По блоку

--- src/js/animation/watchAnimation.ts
+++ src/js/animation/watchAnimation.ts
@@ -1,17 +1,19 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 export default function initWatchAnimation() {
-  gsap.timeline({
-    scrollTrigger: {
-      trigger: '.watch',
-      start: "top 30%",
-      toggleActions: "play none none none" // play при входе, ничего при выходе
-    }
-  })
-  .to('.watch__bg-image', { opacity: 1, duration: 1.2 })
-  .to('.watch__heading', { opacity: 1, y: 0, duration: 0.8 }, '-=0.5')
-  .to('.watch__text', { opacity: 1, y: 0, duration: 1 }, '-=0.3');
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".watch",
+        start: "top 30%",
+        markers: false,
+        toggleActions: "play none none none", // play при входе, ничего при выходе
+      },
+    })
+    .from(".watch__bg-image", { autoAlpha: 0, duration: 1.2 })
+    .from(".watch__heading", { autoAlpha: 0, y: 100, duration: 0.8 }, "-=0.5")
+    .from(".watch__text", { autoAlpha: 0, y: 100, duration: 1 }, "-=0.3");
 }
