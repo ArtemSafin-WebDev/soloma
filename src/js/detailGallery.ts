b3ebabb65a0c461a23412b488998109813ed2707
Artem Safin <artemsafin.webdev@gmail.com>
2025-08-23 19:39:04 +0300

    ПО дивной

--- src/js/detailGallery.ts
+++ src/js/detailGallery.ts
@@ -10,6 +10,14 @@ export default function detailGallery() {
   elements.forEach((element) => {
     let mm = gsap.matchMedia();
     const container = element.querySelector<HTMLElement>(".swiper");
+    const cards = Array.from(
+      element.querySelectorAll<HTMLElement>(".detail-gallery__slider-card")
+    );
+    const wrappers = Array.from(
+      element.querySelectorAll<HTMLElement>(
+        ".detail-gallery__slider-mobile-wrapper-inner"
+      )
+    );
     if (!container) return;
 
     mm.add("(min-width: 641px)", () => {
@@ -33,6 +41,23 @@ export default function detailGallery() {
       });
       return () => {
         instance.destroy();
+      };
+    });
+    const originalParentElements = cards.map((card) => card.parentElement);
+    mm.add("(max-width: 640px)", () => {
+      cards.forEach((card, cardIndex) => {
+        if ((cardIndex + 1) % 2 === 0) {
+          wrappers[1].appendChild(card);
+        } else {
+          wrappers[0].appendChild(card);
+        }
+      });
+
+      return () => {
+        console.log("Returning to parent elements", originalParentElements);
+        cards.forEach((card, cardIndex) => {
+          originalParentElements[cardIndex]?.appendChild(card);
+        });
       };
     });
   });
